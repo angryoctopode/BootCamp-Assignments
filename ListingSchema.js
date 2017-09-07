78ec7c9535bff1a689fa6eb99fad07559b8a4045
Alex Heuser <aheuser@ufl.edu>
2017-09-06 22:36:59 -0400

    First of my commit here

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,15 +1,28 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+  code: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  coordinates: {
+    latitude: {type: Number},
+    longitude: {type: Number}
+  },
+  address: {type: String},
+  created_at: {type: String},
+  updated_at: {type: String}
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+  var currDate = new Date();
+  this.update_at = currDate;
+  if(!this.created_at) {
+      this.created_at = currDate;
+  }
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
