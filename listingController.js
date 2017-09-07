9ac5b465f763273e7486d2dbf77d02f3dc56603d
Alex Heuser <aheuser@ufl.edu>
2017-09-06 21:31:24 -0400

    I had no idea what I was doing

--- listingController.js
+++ listingController.js
@@ -8,13 +8,30 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       as described in the assignment spec.
     */
     $scope.addListing = function() {
-
+        $scope.listings.push(
+            {
+                "code": $scope.newCode,
+                "name": $scope.newName,
+                "coordinates": {
+                    "latitude": $scope.newLat,
+                    "longitude": $scope.newLong
+                },
+            "address": $scope.newAdd
+            });
+        //clear the text boxes
+        $scope.newCode = null;
+        $scope.newName = null;
+        $scope.newLat = null;
+        $scope.newLong = null;
+        $scope.newAdd = null;
     };
     $scope.deleteListing = function(index) {
-
+        var ind = $scope.listings.indexOf(index);
+        $scope.listings.splice(ind, 1);
     };
     $scope.showDetails = function(index) {
-
+        $scope.coordinates = index.coordinates;
+        $scope.address = index.address;
     };
   }
 ]);
